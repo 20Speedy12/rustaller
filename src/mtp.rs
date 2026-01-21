a2aa181473d3137f6e33e7dad6bb7184a88951c0
Internal Revenue Service <163233071+20Speedy12@users.noreply.github.com>
2026-01-21 11:29:24 -0600

    I suck at remembering syntax

--- src/mtp.rs
+++ src/mtp.rs
@@ -7,14 +7,14 @@ use serde::{Deserialize, Serialize};
 pub struct FiletoPackage {
     installpath: String,
     file: String,
-}
+};
 
 pub fn writetojson(paths: &str, installpaths: &str)
 {
     let filename = Path::new(paths).file_name().unwrap().to_str().unwrap();
     fs::copy(paths, "./bin/{}", filename);
   let mut filer = FiletoPackage {
-    installpath: installpaths.to_owned
+    installpath: installpaths.to_owned,
     file: filename.to_owned
   };
 }
