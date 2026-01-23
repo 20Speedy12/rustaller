c14091c98b2f5a95028c457685a1d1bf1739bc78
Internal Revenue Service <163233071+20Speedy12@users.noreply.github.com>
2026-01-23 10:52:42 -0600

    curi
    curry

--- src/main.rs
+++ src/main.rs
@@ -12,4 +12,12 @@ mod mtp;
 //hi :3
 fn main() {
     let mut siv = cursive::default();
+    siv.add_layer(TextView::new(
+        "Hello World!\n\
+         Press q to quit the application.",
+    ));
+
+    siv.add_global_callback('q', Cursive::quit);
+    
+    siv.run
 }
